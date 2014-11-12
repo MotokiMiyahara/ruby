a69441158f433a069def8235ca1dc3bdd6e5c428
motokimiyahara <duos2002@yahoo.co.jp>
2014-11-12 12:45:14 +0900

    refactor remove_news_files.rb

---
+++ crawlers/scripts/helpers/news_file_operation.rb
@@ -0,0 +1,73 @@
+#!/usr/bin/env ruby
+# vim:set fileencoding=utf-8 ts=2 sw=2 sts=2 et:
+
+require_relative 'modules'
+require 'optparse'
+
+require_relative '../commons'
+require_lib 'util'
+require_lib 'pixiv/config'
+require_lib 'moeren/config'
+require_lib 'yande.re/crawler'
+require_lib 'danbooru_clones/gelbooru/config'
+require_lib 'danbooru_clones/konachan/config'
+
+module Scripts; end
+
+class Scripts::Helpers::NewsFileOperation
+  def execute()
+
+    #type = ARGV[0]
+    type = ARGV.shift
+    news_dir = case type
+  when 'pixiv' 
+    Pixiv::NEWS_DIR
+  when 'moeren'
+    Moeren::Config::NEWS_DIR
+  when 'yandere'
+    Yandere::NEWS_DIR
+  when 'gelbooru'
+    Crawlers::DanbooruClones::Gelbooru::Config.news_dir
+  when 'konachan'
+    Crawlers::DanbooruClones::Konachan::Config.news_dir
+  else
+    raise "wrong type=#{type}"
+  end
+
+  should_delete = false
+  opt = OptionParser.new
+  opt.on('-y', 'say yes'){should_delete = true}
+  opt.parse!
+
+  #should_delete ||= say_yes?("Do you want to delete #{news_dir}?")
+  should_operate ||= say_yes?(message(news_dir))
+  exit unless should_operate
+
+  #Crawlers::Util::clean_up_dir(news_dir)
+  operate(news_dir)
+  puts "finish. (#{news_dir})"
+end
+
+def say_yes?(prompt)
+  puts prompt + " [yN]"
+  line = $stdin.gets
+  return line =~ /^y(?:es)?/i 
+end
+
+
+def message(news_dir)
+  raise 'not implemented.'
+end
+
+def operate(news_dir)
+  raise 'not implemented.'
+end
+end
+
+if $0 == __FILE__
+  include Scripts
+  #$script_header(:type)
+  #RemveNewsFiles.execute()
+end
+
+
--- crawlers/scripts/remove_news_files.rb
+++ crawlers/scripts/remove_news_files.rb
@@ -4,6 +4,7 @@
 require 'optparse'
 
 require_relative 'commons'
+require_relative 'helpers/news_file_operation'
 require_lib 'util'
 require_lib 'pixiv/config'
 require_lib 'moeren/config'
@@ -13,52 +14,21 @@ require_lib 'danbooru_clones/konachan/config'
 
 module Scripts; end
 
-class Scripts::RemveNewsFiles
-  class << self
-    def execute()
+class Scripts::RemoveNewsFiles < Scripts::Helpers::NewsFileOperation
 
-      #type = ARGV[0]
-      type = ARGV.shift
-      news_dir = case type
-      when 'pixiv' 
-        Pixiv::NEWS_DIR
-      when 'moeren'
-        Moeren::Config::NEWS_DIR
-      when 'yandere'
-        Yandere::NEWS_DIR
-      when 'gelbooru'
-        Crawlers::DanbooruClones::Gelbooru::Config.news_dir
-      when 'konachan'
-        Crawlers::DanbooruClones::Konachan::Config.news_dir
-      else
-        raise "wrong type=#{type}"
-      end
+  def message(news_dir)
+    return "Do you want to delete #{news_dir}?"
+  end
 
-      should_delete = false
-      opt = OptionParser.new
-      opt.on('-y', 'say yes'){should_delete = true}
-      opt.parse!
-
-      should_delete ||= say_yes?("Do you want to delete #{news_dir}?")
-      exit unless should_delete
-
-      Crawlers::Util::clean_up_dir(news_dir)
-      puts "finish deleting. (#{news_dir})"
-    end
-
-    def say_yes?(prompt)
-      puts prompt + " [yN]"
-      line = $stdin.gets
-      return line =~ /^y(?:es)?/i 
-    end
-
+  def operate(news_dir)
+    Crawlers::Util::clean_up_dir(news_dir)
   end
 end
 
 if $0 == __FILE__
   include Scripts
   script_header(:type)
-  RemveNewsFiles.execute()
+  RemoveNewsFiles.new.execute()
 end
 
 
